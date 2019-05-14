523ba65ce4fe2ca1fe66372a34af74101e2ca279
Axel Boberg <ovrigt@axelboberg.se>
2019-05-14 17:06:23 +0200

    Add test for addResolverAfter

--- test/resolvers.test.js
+++ test/resolvers.test.js
@@ -3,10 +3,17 @@ const routes = require('./test.routes')
 
 const router = new Router(routes)
 
-test('add resolver', async () => {
-  router.addResolver((obj, path, opts, resolve) => {
+test('add resolver before stack', async () => {
+  router.addResolverBefore((obj, path, opts, resolve) => {
     if (!opts.req) opts.req = {}
     opts.req.before = true
   })
   await expect(router.execute('/req')).resolves.toHaveProperty('before', true)
+})
+
+test('add resolver after stack', async () => {
+  router.addResolverAfter((obj, path, opts, resolve) => {
+    resolve(req => Promise.resolve('Appended resolver'))
+  })
+  await expect(router.execute('/req')).resolves.toBe
 })
