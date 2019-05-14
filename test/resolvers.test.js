859006cb4638391d75c3a506b56bb3e19250984a
Axel Boberg <ovrigt@axelboberg.se>
2019-05-14 15:11:31 +0200

    Update resolver signature to be more logical

--- test/resolvers.test.js
+++ test/resolvers.test.js
@@ -3,29 +3,10 @@ const routes = require('./test.routes')
 
 const router = new Router(routes)
 
-test('prepend resolver', async () => {
-  router.addResolverBefore((obj, path, resolve, opts) => {
+test('add resolver', async () => {
+  router.addResolver((obj, path, opts, resolve) => {
     if (!opts.req) opts.req = {}
     opts.req.before = true
   })
   await expect(router.execute('/req')).resolves.toHaveProperty('before', true)
-})
-
-test('append resolver', async () => {
-  router.addResolverAfter((obj, path, resolve, opts) => {
-
-    /**
-     * Resolve to a hijacked object
-     * resolver a get handler
-     */
-    resolve ({
-      'get': req => { return Promise.resolve('hijack') }
-    })
-  })
-
-  /**
-   * Use a non matching route since the appended
-   * resolver wouldn't otherwise be called
-   */
-  await expect(router.execute('/404')).resolves.toBe('hijack')
 })
